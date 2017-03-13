864f2fc2912b3894f4db2f8d8fcf30b037a5f368
mw-voo <necro343@gmail.com>
2017-03-13 15:21:27 -0500

    Revert "Initial Upload"
    This reverts commit 7b2963ca934dc0a90b34d769bd438db576758c05.

--- lua/autorun/customtarget.lua
+++
@@ -1,7 +0,0 @@
-if SERVER then
-	include('customtargets/sv_customtargetting.lua')
-	include('customtargets/sh_definitions.lua')
-	AddCSLuaFile('customtargets/sh_definitions.lua')
-else
-	include('customtargets/sh_definitions.lua')
-end
